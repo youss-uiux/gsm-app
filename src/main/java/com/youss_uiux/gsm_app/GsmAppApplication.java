af2df28790ab748705beb1a9655ead283cf93ab7
youss-uiux <youssif.aboubacar@outlook.fr>
2025-04-18 00:30:45 +0200

    First commit

---
+++ src/main/java/com/youss_uiux/gsm_app/GsmAppApplication.java
@@ -0,0 +1,13 @@
+package com.youss_uiux.gsm_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsmAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsmAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/youss_uiux/gsm_app/GsmAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youss_uiux.gsm_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsmAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
