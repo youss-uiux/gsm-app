8d48ad42f77cf3f28e4740fb204162c66a5a79cb
youss-uiux <youssif.aboubacar@outlook.fr>
2025-04-22 15:05:17 +0200

    Add entity to data

--- src/main/java/com/youss_uiux/gsm_app/models/Gsm.java
+++ src/main/java/com/youss_uiux/gsm_app/models/Gsm.java
@@ -1,16 +1,25 @@
 package com.youss_uiux.gsm_app.models;
 
+import jakarta.persistence.*;
 import lombok.*;
 
+@Entity
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class Gsm {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID) // Ou AUTO si tu veux un Long
     private String id;
+
     private String marque;
     private String modele;
+
     private boolean allume;
+
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn(name = "sim_id", referencedColumnName = "id")
     private Sim sim;
 
     public boolean estJoignable() {
--- src/main/java/com/youss_uiux/gsm_app/models/Sim.java
+++ src/main/java/com/youss_uiux/gsm_app/models/Sim.java
@@ -1,7 +1,12 @@
 package com.youss_uiux.gsm_app.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.*;
 
+@Entity
 @Data
 @Builder
 @NoArgsConstructor
@@ -9,6 +14,10 @@ import lombok.*;
 @Setter
 @Getter
 public class Sim {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
     private String numero;
     private String operateur;
     private int unites;
